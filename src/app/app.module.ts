2c5633340035026010ab28d2787eb173ef213ce4
Julian Aguilar2 <julianaguilar2@guest-wireless-upc-1604-10-120-076-043.usc.edu>
2018-07-07 14:40:56 -0700

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { BlogsComponent } from './components/blogs/blogs.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { ServiceComponent } from './service/service.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BlogsComponent,
+    ContactComponent,
+    ServiceComponent
   ],
   imports: [
     BrowserModule,
